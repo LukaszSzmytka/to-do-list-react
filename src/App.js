fb66672b20411702d0e9a5a78bfd274b97441158
Lukasz Szmytka <szmytka983@gmail.com>
2023-01-08 16:50:39 +0100

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -9,16 +9,27 @@ import { useState } from "react";
 function App() {
   const [hideDone, setHideDone] = useState(false);
   const [tasks, setTasks] = useState([
-      { id: 1, content: "przejść na Reacta", done: false },
-      { id: 2, content: "zjeść obiad", done: true },
-    ]);
+    { id: 1, content: "przejść na Reacta", done: false },
+    { id: 2, content: "zjeść obiad", done: true },
+  ]);
 
   const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
+    setHideDone((hideDone) => !hideDone);
   };
 
   const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleTaskDone = (id) => {
+    setTasks((tasks) =>
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, done: !task.done };
+        }
+        return task;
+      })
+    );
   };
 
   return (
@@ -28,16 +39,18 @@ function App() {
       <Section
         title="Lista zadań"
         body={
-        <List
-         tasks={tasks}
-         hideDone={hideDone}
-         removeTask={removeTask}
-        />}
+          <List
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
         extraHeaderContent={
           <Buttons
-           tasks={tasks}
-           hideDone={hideDone}
-           toggleHideDone={toggleHideDone}
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
           />
         }
       />
--- src/List/index.js
+++ src/List/index.js
@@ -1,20 +1,36 @@
 import "./style.css";
 
-const List = ({ tasks, hideDone, removeTask }) => (
+const List = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <ul className="list">
     {tasks.map((task) => (
-      <li key={task.id} className={`list__items ${task.done && hideDone === true ? " list__items--hidden" : ""}`}>
-        <button className="list__buttons list__buttons--done">
+      <li 
+        key={task.id} 
+        className={`list__items ${
+          task.done && hideDone
+           ? " list__items--hidden"
+           : ""}`
+          }
+      >
+        <button
+         className="list__buttons list__buttons--done"
+         onClick={() => toggleTaskDone(task.id)}
+        >
           {task.done ? "✓" : ""}
         </button>
-        <span className={`list__itemsText ${task.done ? " list__itemsText--done" : ""}`}>
+        <span
+         className={`list__itemsText ${
+          task.done
+           ? " list__itemsText--done"
+           : ""}`
+          }
+        >
           {task.content}
         </span>
         <button
           className="list__buttons list__buttons--delete"
           onClick={() => removeTask(task.id)}
         >
-            🗑️
+          🗑️
         </button>
       </li>
     ))}

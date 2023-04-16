a83f6a81b5dd1a12c17a69f06eea01c505d4e639
Lukasz Szmytka <szmytka983@gmail.com>
2023-04-16 17:43:59 +0200

    focusInput function

--- src/Form/index.js
+++ src/Form/index.js
@@ -5,6 +5,10 @@ const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
   const inputRef = useRef(null);
 
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
   const onFormSubmit = (event) => {
     event.preventDefault();
     addNewTask(newTaskContent.trim());
@@ -14,13 +18,14 @@ const Form = ({ addNewTask }) => {
   return (
     <StyledForm onSubmit={onFormSubmit}>
       <Input
+        autoFocus
         ref={inputRef}
         value={newTaskContent}
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)}
       />
       <Button
-        onClick={ () => inputRef.current.focus() }
+        onClick={focusInput}
       >
         Dodaj zadanie
       </Button>
